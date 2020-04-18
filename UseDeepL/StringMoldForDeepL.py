9670ab6d531e3019b788b1016f0a4ce6e777e3f1
白井優武 <masatakeshirai0814@gmail.com>
2020-04-18 20:37:44 +0900

    スクリプト化

--- UseDeepL/StringMoldForDeepL.py
+++ UseDeepL/StringMoldForDeepL.py
@@ -2,13 +2,14 @@ def StringMoldForDeepL(string):
     mold_string = string.replace('\n',' ').replace('. ','.\n').replace('','fi').replace('','fl')
     return mold_string
 
-import os
-os.chdir('UseDeepL')
+if __name__ == "__main__":
+    import os
+    os.chdir('UseDeepL')
 
-file = open('before.txt', 'r')
-string = file.read()
-file.close()
+    file = open('before.txt', 'r')
+    string = file.read()
+    file.close()
 
-file = open('after.txt', 'w')
-file.write(StringMoldForDeepL(string))
-file.close()
+    file = open('after.txt', 'w')
+    file.write(StringMoldForDeepL(string))
+    file.close()
