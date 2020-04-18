1e6250b76d3f1debfad85ee76268d0cfec62f51f
白井優武 <masatakeshirai0814@gmail.com>
2020-04-18 20:22:50 +0900

    stringの加工をメソッド化

--- UseDeepL/StringMoldForDeepL.py
+++ UseDeepL/StringMoldForDeepL.py
@@ -1,12 +1,14 @@
+def StringMoldForDeepL(string):    
+    mold_string = string.replace('\n',' ').replace('. ','.\n').replace('','fi')
+    return mold_string
+
 import os
-os.chdir('StringMoldForDeepL')
+os.chdir('UseDeepL')
 
 file = open('before.txt', 'r')
 string = file.read()
 file.close()
 
-mold_string = string.replace('\n',' ').replace('. ','.\n').replace('','fi')
-
 file = open('after.txt', 'w')
-file.write(mold_string)
+file.write(StringMoldForDeepL(string))
 file.close()
