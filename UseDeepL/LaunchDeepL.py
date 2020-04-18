138442bb2031d5422f215c8407443c25e524de5c
白井優武 <masatakeshirai0814@gmail.com>
2020-04-18 22:20:07 +0900

    フォームに英文を送信できる

--- UseDeepL/LaunchDeepL.py
+++ UseDeepL/LaunchDeepL.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import os
 
+#あらゆる環境でSeleniumが動作できるように設定
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-extensions')
@@ -18,6 +19,18 @@ options.add_argument('--start-maximized')
 
 #chrome driverへのパス
 DRIVER_PATH = os.path.expanduser('~/chromedriver_win32/chromedriver.exe')
+#スクレイピングするURL
+URL = 'https://www.deepl.com/ja/translator'
 
+#英文を入力するフォームのセレクタ
+textarea = '#dl_translator > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--source > div.lmt__textarea_container > div > textarea'
+
+#chromeを起動
 driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
 
+#DeepLのページへアクセス
+driver.get(URL)
+
+#フォームに英文を入力する
+element = driver.find_element_by_css_selector(textarea)
+element.send_keys('Hello world!')
