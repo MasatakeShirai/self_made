6644d96a9262ccdfb3d529a5839f3cddeb62d95c
白井優武 <masatakeshirai0814@gmail.com>
2020-04-18 21:59:24 +0900

    chromeを起動できる

---
+++ UseDeepL/LaunchDeepL.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import os
+
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-extensions')
+options.add_argument('--proxy-server="direct://"')
+options.add_argument('--proxy-bypass-list=*')
+options.add_argument('--start-maximized')
+
+#chrome driverへのパス
+DRIVER_PATH = os.path.expanduser('~/chromedriver_win32/chromedriver.exe')
+
+driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
+
