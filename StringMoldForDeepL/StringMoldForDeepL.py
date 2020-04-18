6c8a2eb19afb647c73387a2d2771b0aa2b0ba3e9
白井優武 <masatakeshirai0814@gmail.com>
2020-04-18 19:55:35 +0900

    'fi'を補填できる

--- StringMoldForDeepL/StringMoldForDeepL.py
+++ StringMoldForDeepL/StringMoldForDeepL.py
@@ -5,7 +5,7 @@ file = open('before.txt', 'r')
 string = file.read()
 file.close()
 
-mold_string = string.replace('\n',' ').replace('. ','.\n').replace(' rst',' first').replace(' nd', ' find')
+mold_string = string.replace('\n',' ').replace('. ','.\n').replace('','fi')
 
 file = open('after.txt', 'w')
 file.write(mold_string)
