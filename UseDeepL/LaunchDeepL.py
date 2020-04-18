0a248b4a4bbae486a7b4100eebff306862a4a473
白井優武 <masatakeshirai0814@gmail.com>
2020-04-18 23:42:24 +0900

    10秒間待期して，クリップボードに結果をコピーする

--- UseDeepL/LaunchDeepL.py
+++ UseDeepL/LaunchDeepL.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import os
+import time
 
 #あらゆる環境でSeleniumが動作できるように設定
 options = Options()
@@ -23,7 +24,9 @@ DRIVER_PATH = os.path.expanduser('~/chromedriver_win32/chromedriver.exe')
 URL = 'https://www.deepl.com/ja/translator'
 
 #英文を入力するフォームのセレクタ
-textarea = '#dl_translator > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--source > div.lmt__textarea_container > div > textarea'
+before_textarea = '#dl_translator > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--source > div.lmt__textarea_container > div > textarea'
+after_textarea = '#dl_translator > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--target > div.lmt__textarea_container > div.lmt__inner_textarea_container > textarea'
+CopyButton = '#dl_translator > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--target > div.lmt__textarea_container > div.lmt__target_toolbar.lmt__target_toolbar--visible > div.lmt__target_toolbar__copy > button'
 
 #chromeを起動
 driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
@@ -32,5 +35,18 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
 driver.get(URL)
 
 #フォームに英文を入力する
-element = driver.find_element_by_css_selector(textarea)
+element = driver.find_element_by_css_selector(before_textarea)
 element.send_keys('Hello world!')
+
+#訳文が表示されるまで待期
+'''
+element = WebDriverWait(driver, 30).until(
+	EC.element_to_be_clickable((By.CSS_SELECTOR, after_textarea))
+)
+'''
+time.sleep(10)
+
+#クリップボードにコピーする
+element = driver.find_element_by_css_selector(CopyButton)
+driver.execute_script('arguments[0].click();',element)
+
