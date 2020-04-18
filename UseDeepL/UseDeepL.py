8ae8bab724dd4a54943c7fe521883d3b48ff4835
白井優武 <masatakeshirai0814@gmail.com>
2020-04-19 03:25:01 +0900

    LaunchDeepLとStringMoldForDeepLを利用するスクリプト

---
+++ UseDeepL/UseDeepL.py
@@ -0,0 +1,8 @@
+from LaunchDeepL import LaunchDeepL
+from StringMoldForDeepL import StringMoldForDeepL
+import sys 
+sys.dont_write_bytecode = True
+
+print(StringMoldForDeepL('abc\ndef'))
+LaunchDeepL('hello')
+
