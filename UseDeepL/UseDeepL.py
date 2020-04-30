9e778622b190ef6dd33d16ec3fc5618cd9338b89
白井優武 <masatakeshirai0814@gmail.com>
2020-04-30 21:57:20 +0900

    ファイル読み出し時のエンコードを指定

--- UseDeepL/UseDeepL.py
+++ UseDeepL/UseDeepL.py
@@ -12,10 +12,10 @@ import sys
 
 #スクリプト名以外の引数を取得
 argv =sys.argv[1:]
-options, arguments = getopt.getopt(argv, "hg", ["help", "generate"])
+options, arguments = getopt.getopt(argv, "hg", ["help", "generate","generate="])
 
 #option_dictはオプションによる設定のまとめ
-option_dict = {'help':False, 'generate':[False,None]}
+option_dict = {'help':False, 'generate':[False,'after.txt']}
 
 #引数を読んで，option_dictのフラグを立てる
 for name,value in options:
@@ -24,7 +24,7 @@ for name,value in options:
     if name in ('-g','--generate'):
         option_dict['generate'][0] = True
         if value is not None:
-            option_dict['generate'][1] = name
+            option_dict['generate'][1] = value
 
 if option_dict['help']:
     print('before.txtに英文を保存してこのスクリプトを実行すると，DeepLのサイトをスクレイピングして翻訳をクリップボードにコピーします．')
@@ -33,8 +33,8 @@ if option_dict['help']:
     sys.exit()
 
 os.chdir('UseDeepL')
-
-file = open('before.txt', 'r')
+    
+file = open('before.txt', 'r', encoding='utf-8')
 string = file.read()
 file.close()
 
@@ -45,6 +45,6 @@ pyperclip.copy(translate_string)
 
 #結果をtxtファイルに出力する
 if option_dict['generate'][0]:
-    file = open('after.txt','w',encoding='utf-8')
+    file = open(option_dict['generate'][1],'w',encoding='utf-8')
     file.write(translate_string)
     file.close()
