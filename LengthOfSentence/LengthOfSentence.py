89ff7df7c81de4459565b94b0b1ed82482009854
白井優武 <masatakeshirai0814@gmail.com>
2020-06-09 20:35:21 +0900

    文字列から改行を削除して文字数を表示する

--- LengthOfSentence/LengthOfSentence.py
+++ LengthOfSentence/LengthOfSentence.py
@@ -3,5 +3,9 @@ import os
 os.chdir("LengthOfSentence")
 
 file = open('target.txt', 'r', encoding='utf-8')
-print(file.readline())
+
+s = file.read().replace('\n','')
+
+print(len(s))
+
 file.close()
