33996e57a8b0d5c5e8ee2b0295a24b858cf77602
白井優武 <masatakeshirai0814@gmail.com>
2020-06-17 13:48:23 +0900

    ffに対応できる

--- UseDeepL/StringMold.py
+++ UseDeepL/StringMold.py
@@ -2,7 +2,7 @@ import os
 
 def StringMoldForDeepL(string):
     #文字列を整形する    
-    mold_string = string.replace('\n',' ').replace('. ','.\n').replace('','fi').replace('','fl')
+    mold_string = string.replace('\n',' ').replace('. ','.\n').replace('','fi').replace('','fl').replace('','ff')
  
     return mold_string
 
